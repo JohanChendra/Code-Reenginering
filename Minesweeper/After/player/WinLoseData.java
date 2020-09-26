fda3550e0a30a1e282e5658b29bb350004364086
JohanChendra <71887053+JohanChendra@users.noreply.github.com>
2020-09-26 13:44:29 +0700

    Add files via upload

---
+++ Minesweeper/After/player/WinLoseData.java
@@ -0,0 +1,19 @@
+package player;
+
+public abstract class WinLoseData{
+	protected int win;
+	protected int lose;
+	
+	public int getWin() {
+		return win;
+	}
+	public void setWin(int win) {
+		this.win = win;
+	}
+	public int getLose() {
+		return lose;
+	}
+	public void setLose(int lose) {
+		this.lose = lose;
+	}
+}
